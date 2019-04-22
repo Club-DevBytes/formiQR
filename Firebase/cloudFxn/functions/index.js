fa34bb521d4a9121c1d1935391e703496b59b968
Akshay090 <aks28id@gmail.com>
2019-04-22 16:18:32 +0530

    cloud Fxn Redundant code removed

--- Firebase/cloudFxn/functions/index.js
+++ Firebase/cloudFxn/functions/index.js
@@ -19,7 +19,7 @@ const mailTransport = nodemailer.createTransport({
   }
 });
 
-exports.makerFxn = functions.database
+exports.mailFxn = functions.database
   .ref("/maker/{id}")
   .onCreate((snapshot, context) => {
     // Grab the current value of what was written to the Realtime Database.
@@ -29,8 +29,7 @@ exports.makerFxn = functions.database
 
     const makerID = context.params.id;
 
-    // return snapshot.ref.parent.parent.child('confMaker')
-    // .child(makerID).child(makers.name).set(makers.stamp);
+
 
     //firebase functions:config:set gmail.email=myemailID@gmail.com gmail.password=Mypassword
     //To set email and password
@@ -52,11 +51,11 @@ function sendEmail(makers, makerID) {
   // var UNIQUE_ID = makerID;
 
   var UNIQUE_QR = `https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=${+makerID + 1000}`
-  
+
 
   var filePath = path.join(__dirname, 'templates/makerEmail.html');
 
-  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+  fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
     data = data.toString();
     data = data.replace(/##UNIQUE_NAME/g, UNIQUE_NAME);
     // data = data.replace(/##UNIQUE_ID/g, UNIQUE_ID);
@@ -64,47 +63,37 @@ function sendEmail(makers, makerID) {
 
 
     var mailOptions = {
-        from: '"MakerFest" <noreply@firebase.com>', // sender address 
-        to: makers.email, // list of receivers 
-        subject: 'Thank You For Registration', // Subject line 
-        html: data // html body
+      from: '"MakerEvent" <noreply@firebase.com>', // sender address 
+      to: makers.email, // list of receivers 
+      subject: 'Thank You For Registration', // Subject line 
+      html: data // html body
     };
 
 
-  // const mailOptions = {
-  //   from: '"Aks Corp." <noreply@firebase.com>',
-  //   to: makers.email,
-  // };
-  // // Building Email message.
-  // mailOptions.subject = "Thanks and Welcome!";
-  // mailOptions.text =
-  // "Thanks you for subscribing to our newsletter. You will receive our next weekly newsletter.";
 
-  
+    try {
+      mailTransport.sendMail(mailOptions);
+    } catch (error) {
+      console.error('There was an error while sending the email:', error);
 
-  try {
-    mailTransport.sendMail(mailOptions);
-  } catch(error) {
-    console.error('There was an error while sending the email:', error);
+      // errorEmails = functions.database.ref(`/emailError/${makerID}`).set({
+      //   email: makers.email
+      // })
 
-    // errorEmails = functions.database.ref(`/emailError/${makerID}`).set({
-    //   email: makers.email
-    // })
-
-  }
+    }
 
 
-  return console.log(
-    `Sending mail to ${makers.name} with stamp ${makers.stamp}`
-  );
-  }); 
+    return console.log(
+      `Sending mail to ${makers.name} with stamp ${makers.stamp}`
+    );
+  });
 }
 
 //To deploy firebase deploy --only functions:makerFxn
 
 
 
-exports.visitorFxn = functions.database
+exports.genderCountFxn = functions.database
   .ref("/atEvent/{date}/{id}")
   .onCreate((snapshot, context) => {
     // Grab the current value of what was written to the Realtime Database.
@@ -113,61 +102,24 @@ exports.visitorFxn = functions.database
     console.log(Visitor);
 
     console.log(Visitor.name);
-    
-//---------------------------------------------------
-//Working gender counter
+
+    //---------------------------------------------------
+    //Working gender counter
     const genderCountersRef = admin
-    .database()
-    .ref(`count/${Visitor.gender}`);
+      .database()
+      .ref(`count/${Visitor.gender}`);
 
     return genderCountersRef
-    .transaction(counter_value => {
-      return (counter_value || 0) + 1;
-    })
-//-----------------------------------------------
-    // const overallCountersRef = admin
-    // .database()
-    // .ref(`count/overAllTotal`);
-
-    // return overallCountersRef
-    // .transaction(counter_value => {
-    //   return (counter_value || 0) + 1;
-    // })
-
-    
-
-    // const genderRef = admin
-    // .database()
-    // .ref(`count/${makers.gender}`);  
-    
-    // return countersRef.transaction(currentData => {
-    //   // currentData= (currentData || 0) + 1;
-    //   console.log("Inside currentData Fxn");
-
-    //   console.log(currentData+" llllll"+ Visitor.gender+" "+ countersRef);
-    //   // currentData[Visitor.gender] = (currentData[Visitor.gender] || 0) + 1;
-
-    // if (currentData !== null) {
-    //   currentData[Visitor.gender] = (currentData[Visitor.gender] || 0) + 1;
-    //   return currentData;
-    // }
-
-    // });
-    // return snapshot.ref.parent.parent.child('confMaker')
-    // .child(makerID).child(makers.name).set(makers.stamp);
-
-  
-
-    //To view the seted email and pass firebase functions:config:get
+      .transaction(counter_value => {
+        return (counter_value || 0) + 1;
+      })
 
   });
-  //To deploy firebase deploy --only functions:visitorFxn
- 
+//To deploy firebase deploy --only functions:visitorFxn
 
 
 
-
-  exports.overallFxn = functions.database
+exports.overallCountFxn = functions.database
   .ref("/atEvent/{date}/{id}")
   .onCreate((snapshot, context) => {
     // Grab the current value of what was written to the Realtime Database.
@@ -176,9 +128,9 @@ exports.visitorFxn = functions.database
     console.log(Visitor);
 
     console.log(Visitor.name);
-    
-//---------------------------------------------------
-//Working gender counter
+
+    //---------------------------------------------------
+    //Working gender counter
     // const genderCountersRef = admin
     // .database()
     // .ref(`count/${Visitor.gender}`);
@@ -187,15 +139,15 @@ exports.visitorFxn = functions.database
     // .transaction(counter_value => {
     //   return (counter_value || 0) + 1;
     // })
-//-----------------------------------------------
+    //-----------------------------------------------
     const overallCountersRef = admin
-    .database()
-    .ref(`count/overAllTotal`);
+      .database()
+      .ref(`count/overAllTotal`);
 
     return overallCountersRef
-    .transaction(counter_value => {
-      return (counter_value || 0) + 1;
-    })
+      .transaction(counter_value => {
+        return (counter_value || 0) + 1;
+      })
 
-    
+
   });
